cb01436ef63c03eafd6548b075460e5f8b0cd965
TANG ZhiXiong <dvorak4tzx@qq.com>
2016-10-11 00:56:40 +0800

    done

--- src/main8d.cpp
+++ src/main8d.cpp
@@ -1,25 +1,6 @@
-#include <algorithm>
-#include <cctype>
-#include <climits>
-#include <cmath>
-#include <cstddef>
+#include <queue>
 #include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <ctime>
-#include <iostream>
-#include <iterator>
-#include <limits>
-#include <map>
-#include <new>
-#include <numeric>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <string>
-#include <vector>
-/*
-*/
+#include <unordered_set>
 
 #define LOCAL 1
 // #define LOCAL 0
@@ -28,52 +9,66 @@
 #include "Configs.h"
 #endif
 
-int H, W;
-char pic[501][501];
-
+using namespace std;
 #define min(a,b) ((a)<(b)?(a):(b))
 #define max(a,b) ((a)>(b)?(a):(b))
+#define MAXN 501
+#define DOT '#'
+#define CLR 'X'
+#define BEG '@'
+#define END '@'
+#define MID '@'
 
-int u, d, l, r;
-void bfs_udlr( int x, int y, int &px, int &py ) {
-    if ( x < 0 || x >= H || y < 0 || y >= W || pic[x][y] != '#') { return; }
-    pic[x][y] = 'X';
-    if (x < u) { px = x; py = y; }
-    l = min(l, x); r = max(r, x);
-    u = min(u, x); d = max(d, x);
-    bfs_udlr(x + 1, y, px, py);
-    bfs_udlr(x - 1, y, px, py);
-    bfs_udlr(x,    y+1, px, py);
-    bfs_udlr(x,    y-1, px, py);
-    bfs_udlr(x+1, y+1, px, py);
-    bfs_udlr(x+1, y-1, px, py);
-    bfs_udlr(x-1, y+1, px, py);
-    bfs_udlr(x-1, y-1, px, py);
+int H, W;
+char pic[MAXN][MAXN];
+const int dx[] = { 1, 1, 1, -1, -1, -1, 0, 0 };
+const int dy[] = { 1,-1, 0,  1, -1,  0, 1,-1 };
+
+struct Point {
+    int x, y;
+    Point( int x = 0, int y = 0 ) : x(x), y(y) { }
+    bool valid() const {
+        return 0 <= x && x < H && 0 <= y && y < W;
+    }
+    bool operator==( const Point &rhs ) const {
+        return x == rhs.x && y == rhs.y;
+    }
+};
+
+namespace std {
+    template<> struct hash<Point> {
+        size_t operator()( const Point &p ) const {
+            return p.x * MAXN + p.y;
+        }
+    };
 }
 
-int vis[800][800];
-void resetvis() {
-    for (int i = 0; i < 501; ++i) {
-        for (int j = 0; j < 501; ++j) {
-            vis[i][j] = 100000;
-        }
+void coloring( Point sp, Point &hp, int &u, int &d, int &l, int &r ) {
+    if ( !sp.valid() || pic[sp.x][sp.y] != DOT ) { return; }
+    pic[sp.x][sp.y] = CLR;
+    if( sp.x < u ) { hp = sp; }
+    u = min(u, sp.x); d = max(d, sp.x);
+    l = min(l, sp.y); r = max(r, sp.y);
+    for( int i = 0; i < sizeof(dx)/sizeof(dx[0]); ++i ) {
+        coloring( Point(sp.x+dx[i],sp.y+dy[i]), hp, u, d, l, r );
     }
 }
 
-int maxdist;
-void bfs_endpoint(int x, int y, int dist, int &edpx, int &edpy ) {
-    if ( pic[x][y] != 'X') { return; }
-    if (vis[x][y] <= dist) { return; }
-    vis[x][y] = dist;   // smaller
-    if (dist > maxdist) { edpx = x; edpy = y; maxdist = dist; }
-    bfs_endpoint(x + 1, y, dist + 1, edpx, edpy);
-    bfs_endpoint(x - 1, y, dist + 1, edpx, edpy);
-    bfs_endpoint(x, y+1, dist + 1, edpx, edpy);
-    bfs_endpoint(x, y-1, dist + 1, edpy, edpy);
-    bfs_endpoint(x + 1, y+1, dist + 2, edpx, edpy);
-    bfs_endpoint(x + 1, y-1, dist + 2, edpx, edpy);
-    bfs_endpoint(x - 1, y+1, dist + 2, edpx, edpy);
-    bfs_endpoint(x - 1, y-1, dist + 2, edpx, edpy);
+void reachingout( Point sp, Point &ep ) {
+    unordered_set<Point> s;
+    queue<Point> q;
+    q.push( sp );
+    while( !q.empty() ) {
+        Point p = q.front(); q.pop();
+        for( int i = 0; i < sizeof(dx)/sizeof(dx[0]); ++i ) {
+            Point np(p.x+dx[i], p.y+dy[i]);
+            if( np.valid() && pic[np.x][np.y] == CLR && !s.count(np) ) {
+                s.insert( np );
+                q.push( np );
+            }
+        }
+        ep = p;
+    }
 }
 
 using namespace std;
@@ -85,36 +80,36 @@ int main()
     }
 #endif
     while( 2 == scanf("%d %d", &H, &W) ) {
-        for (int i = 0; i < H; ++i) {
-            scanf("%s", pic[i]);
+        for( int i = 0; i < H; ++i ) {
+            scanf( "%s", pic[i] );
         }
         int countM = 0;
         int countS = 0;
-        for (int x = 0; x < H; ++x) {
-            for (int y = 0; y < W; ++y) {
-                if (pic[x][y] != '#') { continue; }
-                u = 5000; d = -1; l = 5000; r = -1;
-                int px, py;
-                bfs_udlr(x, y, px, py);
-                // printf("px: %d, py: %d\n", px, py);
-                int ep1x, ep1y, ep2x, ep2y;
-                resetvis(); maxdist = -1;
-                bfs_endpoint(px, py, 0, ep1x, ep1y);
-                resetvis(); maxdist = -1;
-                bfs_endpoint(ep1x, ep1y, 0, ep2x, ep2y);
-                // printf("p1: (%d, %d) p2: (%d, %d)\n", ep1x, ep1y, ep2x, ep2y);
-                int ep3x = (ep1x + ep2x) / 2;
-                int ep3y = (ep1y + ep2y) / 2;
-                int midx = (l + r) / 2;
-                int midy = (u + d) / 2;
-                int dx = abs(midx - ep3x);
-                int dy = abs(midy - ep3y);
-                if (dx + dy < (r - l) / 3) {
+        for( int x = 0; x < H; ++x ) {
+            for( int y = 0; y < W; ++y ) {
+                if( pic[x][y] != DOT ) { continue; }
+                int u = H, d = 0, l = W, r = 0;
+                Point sp( x, y ), hp( x, y );
+                coloring( sp, hp, u, d, l, r );
+                Point center( (u+d)/2, (l+r)/2 );
+                Point ep1, ep2, mid;
+                reachingout( hp,  ep1 );
+                reachingout( ep1, ep2 );
+                mid = Point( (ep1.x+ep2.x)/2, (ep1.y+ep2.y)/2 );
+                pic[mid.x][mid.y] = MID;
+                pic[ep1.x][ep1.y] = BEG;
+                pic[ep2.x][ep2.y] = END;
+                Point &m = mid;
+                Point &c = center;
+                if( abs(m.x-c.x)+abs(m.y-c.y) < ((r-l)+(d-u)) / 4 ) {
                     ++countS;
                 } else {
                     ++countM;
                 }
             }
+        }
+        for( int i = 0; i < H; ++i ) {
+            fprintf( stderr, "%s\n", pic[i] );
         }
         printf("%d %d\n", countM, countS);
     }
